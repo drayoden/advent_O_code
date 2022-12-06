5e09e82849cf1de48c4a9d1af2824a0306151e12
daryn <me@daryn.dev>
2022-12-06 10:08:37 -0800

    day 6 - p1 & p2

--- 2022/D06/p1.py
+++ 2022/D06/p1.py
@@ -1,5 +1,35 @@
 import sys
-lines = sys.stdin.readlines()
+import itertools
 
-for l in lines:
+stream = sys.stdin.read()
+
+buff = []
+foundit = 0
+buffsize = 3 # base0 actually 4
+
+for i, c in enumerate(stream):
+
+    #fill the buffer with four chars from the stream
+    buff.append(c)
+    if i >= buffsize:
+        print(buff)
+        diff = True
+        for x,y in itertools.combinations(buff,2):
+            # print(x,y)
+            if x == y:
+                diff = False
+                break
+        if diff:
+            foundit = i
+            break
+        buff.pop(0) # remove first element of list
+        
+
+if foundit != 0: 
+    print("marker found at " + str(foundit + 1))
+else:
+    print("no marker found...")    
+        
+        
+    
 
